44eb7cbde4d29482970c0b6794640ddb58a93ccc
José Gustavo <108258194+gustavocodigo@users.noreply.github.com>
2023-06-03 10:36:08 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+function on_icon_clicked(element) {
+    let elementsChild = document.getElementById("description").childNodes
+    hideNodes(elementsChild)
+    if ( element == document.getElementById("phone")) {
+        
+    }else if ( element == document.getElementById("email")) {
+        
+    }else  if ( element == document.getElementById("about")) {
+
+        
+
+    }else  if ( element == document.getElementById("call")) {
+        
+    }
+}
